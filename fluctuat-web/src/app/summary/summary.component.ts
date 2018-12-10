770da9f1ec4c160ef18afd19670c2cd038841c9b
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2018-12-10 18:08:18 +0100

    feat(web): show informations into summary page

--- fluctuat-web/src/app/summary/summary.component.ts
+++ fluctuat-web/src/app/summary/summary.component.ts
@@ -1,34 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContractService } from '../form-new-delivery/contract.service';
 import { DeliveryService } from '../form-new-delivery/delivery.service';
 import { TransporterService } from '../form-transporter/transporter.service';
+import { Transporter } from '../form-transporter/transporter';
+import { Delivery } from '../form-new-delivery/delivery';
 
 @Component({
   selector: 'flu-summary',
   templateUrl: './summary.component.html',
   styleUrls: [ './summary.component.sass' ]
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
 
-  contractId: string;
+  contract: {id: string, delivery: Delivery, transporter: Transporter};
 
   constructor(private contractService: ContractService,
               private transporterService: TransporterService,
               private deliveryService: DeliveryService) {
   }
 
-  send() {
-    let contract = {
+  ngOnInit() {
+    this.contract = {
       delivery: this.deliveryService.get(),
       transporter: this.transporterService.get(),
-      id: this.contractId
-    };
+    }
+  }
 
-    this.contractService.create(contract).subscribe((location) => {
-      this.contractId = location.split('/').pop();
+  send() {
+    this.contractService.create(this.contract).subscribe((location) => {
+      this.contract.id = location.split('/').pop();
       console.log(location);
       window.open(location, '_blank');
     });
+  }
+
+  printAddress(address) {
+    return `${address.street}, ${address.zipCode} ${address.city}`
   }
 
 }
