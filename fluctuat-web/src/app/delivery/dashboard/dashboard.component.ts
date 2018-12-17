85b0ebed28200ea229a2568606bc8a69302360f3
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2018-12-17 19:07:38 +0100

    feat(web): add dashboard component

--- fluctuat-web/src/app/delivery/dashboard/dashboard.component.ts
+++ fluctuat-web/src/app/delivery/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/index';
+import { shareReplay } from 'rxjs/internal/operators';
+import { Contract } from '../contract';
+import { ContractService } from '../contract.service';
 
 @Component({
   selector: 'flu-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.sass']
+  templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  contracts$: Observable<Contract[]>;
+
+  constructor(private contractService: ContractService) {
+  }
 
   ngOnInit() {
+    this.contracts$ = this.contractService.getAll().pipe(
+      shareReplay(1)
+    );
   }
 
 }
