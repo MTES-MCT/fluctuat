f2b351564531b8644dcba99906d30418197f6dbe
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2018-12-26 11:08:13 +0100

    feat(web): format dates

--- fluctuat-web/src/app/app.component.ts
+++ fluctuat-web/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<flu-header></flu-header>
-             <router-outlet></router-outlet>`,
+  template: `
+    <flu-header></flu-header>
+    <router-outlet></router-outlet>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    registerLocaleData(localeFr);
+  }
 }
