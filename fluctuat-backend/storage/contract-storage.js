658b09b12660637fc4002369863ac0f09382de5c
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2018-12-28 17:37:04 +0100

    feat(backend): dirty persistance in the fs

--- fluctuat-backend/storage/contract-storage.js
+++ fluctuat-backend/storage/contract-storage.js
@@ -1,24 +1,53 @@
-let contracts = [];
+const fs = require('fs');
+const path = require('path');
 
-const put = (contract) => {
-  contracts.push(contract);
+const checkDataDir = (dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, 0o755);
+    console.log('Create data directory', dir);
+  } else {
+    console.log('Data directory OK', dir);
+  }
 };
 
-const getAll = () => contracts;
+checkDataDir('.data');
+const contractsFile = path.join('.data', 'contracts.json');
 
-const count = () => contracts.length;
+const checkContractsFile = () => {
+  if (!fs.existsSync(contractsFile)) {
+    fs.writeFileSync(contractsFile, '[]', 'utf-8');
+    console.log('Create contracts file');
+  }
+};
 
-const get = (id) => contracts[id];
+checkContractsFile();
+
+const getContracts = () => JSON.parse(fs.readFileSync(contractsFile));
+const putContracts = (contracts) => fs.writeFileSync(contractsFile, JSON.stringify(contracts, null, 2));
+
+const count = () => getContracts().length;
+
+const get = (id) => getContracts()[id];
+
+const getAll = () => getContracts();
 
 const patch = (id, values) => {
+  let contracts = getContracts();
   let contract = contracts[id];
   Object.assign(contract, values);
+  putContracts(contracts);
+};
+
+const put = (contract) => {
+  let contracts = getContracts();
+  contracts.push(contract);
+  putContracts(contracts);
 };
 
 module.exports = {
-  patch: patch,
-  put: put,
   count: count,
+  get: get,
   getAll: getAll,
-  get: get
+  patch: patch,
+  put: put
 };
