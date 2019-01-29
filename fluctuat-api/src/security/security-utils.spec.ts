c19ffd7a088c6db5b1177e21020408f4ea44d0c7
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-01-29 15:52:13 +0100

    test: add backent test

---
+++ fluctuat-api/src/security/security-utils.spec.ts
@@ -0,0 +1,8 @@
+import { generateHash, isPasswordMatch } from './security-utils';
+
+describe('security-utils tests', () => {
+
+  it('should match generated hash with password', () => {
+    expect(isPasswordMatch('password', generateHash('password'))).toBeTruthy();
+  })
+});
