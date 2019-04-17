7b2e56aaa20d0deee2e738d04851036fb983ee4d
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-04-17 14:02:57 +0200

    refactor(pdf): minor optimization

--- fluctuat-api/src/pdf/generate-pdf.ts
+++ fluctuat-api/src/pdf/generate-pdf.ts
@@ -1,28 +1,25 @@
 // Based on https://medium.com/@kainikhil/nodejs-how-to-generate-and-properly-serve-pdf-6835737d118e
-import * as pdfMakePrinter from 'pdfmake/src/printer';
+import * as PdfMakePrinter from 'pdfmake/src/printer';
+
+const fonts = {
+  Roboto: {
+    normal: './fonts/Roboto-Regular.ttf',
+    bold: './fonts/Roboto-Bold.ttf'
+  }
+};
+
+const printer = new PdfMakePrinter(fonts);
 
 export function generatePdf(docDefinition) {
-  const fonts = {
-    Roboto: {
-      normal: './fonts/Roboto-Regular.ttf',
-      bold: './fonts/Roboto-Bold.ttf'
-    }
-  };
   return new Promise((success, reject) => {
     try {
-
-      const printer = new pdfMakePrinter(fonts);
       const doc = printer.createPdfKitDocument(docDefinition);
 
       const chunks = [];
 
-      doc.on('data', (chunk) => {
-        chunks.push(chunk);
-      });
+      doc.on('data', (chunk) => chunks.push(chunk));
 
-      doc.on('end', () => {
-        success(Buffer.concat(chunks));
-      });
+      doc.on('end', () => success(Buffer.concat(chunks)));
 
       doc.end();
 
