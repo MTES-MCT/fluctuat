3ba89437063c3b08de4c8cb979ffd366169d4cab
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-04-26 15:23:56 +0200

    feat(scripts): improve export/import db scripts

--- fluctuat-api/scripts/export-db.js
+++ fluctuat-api/scripts/export-db.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
+/**
+ * Export all collections to ./data directory.
+ * Script requires mongoexport are installed
+ */
+const {execSync} = require('child_process');
+const path = require('path');
 
-const { execSync } = require('child_process');
-const configMongo = require('../.data/config.json').mongodb;
+const configPath = path.join(__dirname, '../.data/config.json')
+const configMongo = require(configPath).mongodb;
 
-const uri = `mongodb+srv://${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
+let protocol = configMongo.isDns ? 'mongodb+srv://' : 'mongodb://';
+let uri = `${protocol}${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
+const timestamp = new Date().getTime().toString()
+const outDir = path.join(__dirname, '../.data', configMongo.dbName, timestamp);
 
-execSync(`mongoexport --uri ${uri} --collection waybills --jsonArray --pretty --out ../.data/waybills.json`);
-execSync(`mongoexport --uri ${uri} --collection users --jsonArray --pretty --out ../.data/users.json`);
+execSync(`mongoexport --uri ${uri} --collection users --jsonArray --pretty --out ${outDir}/users.json`);
+execSync(`mongoexport --uri ${uri} --collection waybills --jsonArray --pretty --out  ${outDir}/waybills.json`);
+execSync(`mongoexport --uri ${uri} --collection loadvalidations --jsonArray --pretty --out ${outDir}/loadvalidations.json`);
+execSync(`mongoexport --uri ${uri} --collection unloadvalidations --jsonArray --pretty --out ${outDir}/unloadvalidations.json`);
--- fluctuat-api/scripts/import-db.js
+++ fluctuat-api/scripts/import-db.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
+/**
+ * Import data collections from a source directory'
+ *
+ * usage:  ./import-db.js [SOURCE_DIR]
+ *
+ * SOURCE_DIR: path to exported collections
+ *
+ * Script requires mongoimport are installed
+ */
+const {execSync} = require('child_process');
+const path = require('path');
 
-const { execSync } = require('child_process');
-const configMongo = require('../.data/config.json').mongodb;
+const configPath = path.join(__dirname, '../.data/config.json')
+const configMongo = require(configPath).mongodb;
 
-const uri = `mongodb+srv://${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
+let source = process.argv[2];
+if (!source) {
+  console.error('Missing source directory. Usage: node import-db.js [SOURCE_DIR]')
+  process.exit(1)
+}
 
+let protocol = configMongo.isDns ? 'mongodb+srv://' : 'mongodb://';
+const uri = `${protocol}${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
 // upsert replaces existing documents in the database with matching documents from the import file
 
-execSync(`mongoimport --uri ${uri} --collection waybills --mode upsert --jsonArray --file ../.data/waybills.json`);
-execSync(`mongoimport --uri ${uri} --collection users  --mode upsert --jsonArray --file ../.data/users.json`);
+execSync(`mongoimport --uri ${uri} --collection waybills --mode upsert --jsonArray --file ${source}/waybills.json`);
+execSync(`mongoimport --uri ${uri} --collection users  --mode upsert --jsonArray --file ${source}/users.json`);
+execSync(`mongoimport --uri ${uri} --collection loadvalidations  --mode upsert --jsonArray --file ${source}/loadvalidations.json`);
+execSync(`mongoimport --uri ${uri} --collection unloadvalidations  --mode upsert --jsonArray --file ${source}/unloadvalidations.json`);
