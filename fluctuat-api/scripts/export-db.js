7484507290c0676f7575e77ae757de1344af39ea
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-05-15 18:18:01 +0200

    feat(config): import/export scripts

--- fluctuat-api/scripts/export-db.js
+++ fluctuat-api/scripts/export-db.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 /**
  * Export all collections to ./data directory.
+ *
+ * usage:  ./export-db.js [ENV]
+ *
+ * ENV: (optional) env name if not specified default env is used
+ *
  * Script requires mongoexport are installed
  */
 const {execSync} = require('child_process');
 const path = require('path');
+let env = process.argv[2];
+const ENV_FILE_PATH = env ? path.join(__dirname, `../.env.${env}`) : path.join(__dirname, '../.env');
+const loadConfig = require('dotenv').config(({path: ENV_FILE_PATH}));
 
-const configPath = path.join(__dirname, '../.data/config.json')
-const configMongo = require(configPath).mongodb;
+if (loadConfig.error) {
+  throw loadConfig.error
+}
+const config = loadConfig.parsed;
 
-let protocol = configMongo.isDns ? 'mongodb+srv://' : 'mongodb://';
-let uri = `${protocol}${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
-const timestamp = new Date().getTime().toString()
-const outDir = path.join(__dirname, '../.data', configMongo.dbName, timestamp);
+let protocol = config.DB_IS_DNS === 'true' ? 'mongodb+srv://' : 'mongodb://';
+let uri = `${protocol}${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_CLUSTER}/${config.DB_NAME}`;
+const timestamp = new Date().toISOString().replace(/[:|.]/g, '_');
+const outDir = path.join(__dirname, '../.data', config.DB_NAME, timestamp);
+console.log(`exporting ${config.DB_NAME} db...`)
 
 execSync(`mongoexport --uri ${uri} --collection users --jsonArray --pretty --out ${outDir}/users.json`);
 execSync(`mongoexport --uri ${uri} --collection waybills --jsonArray --pretty --out  ${outDir}/waybills.json`);
--- fluctuat-api/scripts/import-db.js
+++ fluctuat-api/scripts/import-db.js
@@ -2,17 +2,23 @@
 /**
  * Import data collections from a source directory'
  *
- * usage:  ./import-db.js [SOURCE_DIR]
+ * usage:  ./import-db.js SOURCE_DIR [ENV]
  *
  * SOURCE_DIR: path to exported collections
+ * ENV: (optional) env name if not specified default env is used
  *
  * Script requires mongoimport are installed
  */
 const {execSync} = require('child_process');
 const path = require('path');
+let env = process.argv[3];
+const ENV_FILE_PATH = env ? path.join(__dirname, `../.env.${env}`) : path.join(__dirname, '../.env');
+const loadConfig = require('dotenv').config(({path: ENV_FILE_PATH}));
 
-const configPath = path.join(__dirname, '../.data/config.json')
-const configMongo = require(configPath).mongodb;
+if (loadConfig.error) {
+  throw loadConfig.error
+}
+const config = loadConfig.parsed;
 
 let source = process.argv[2];
 if (!source) {
@@ -20,10 +26,12 @@ if (!source) {
   process.exit(1)
 }
 
-let protocol = configMongo.isDns ? 'mongodb+srv://' : 'mongodb://';
-const uri = `${protocol}${configMongo.user}:${configMongo.password}@${configMongo.cluster}/${configMongo.dbName}`;
-// upsert replaces existing documents in the database with matching documents from the import file
+let protocol = config.DB_IS_DNS === 'true' ? 'mongodb+srv://' : 'mongodb://';
+const uri = `${protocol}${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_CLUSTER}/${config.DB_NAME}`;
 
+console.log(`importing to ${config.DB_NAME} db...`)
+
+// upsert replaces existing documents in the database with matching documents from the import file
 execSync(`mongoimport --uri ${uri} --collection waybills --mode upsert --jsonArray --file ${source}/waybills.json`);
 execSync(`mongoimport --uri ${uri} --collection users  --mode upsert --jsonArray --file ${source}/users.json`);
 execSync(`mongoimport --uri ${uri} --collection loadvalidations  --mode upsert --jsonArray --file ${source}/loadvalidations.json`);
