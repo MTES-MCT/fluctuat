3cc5eb878ce71de98e53416b957ecd118f1fc1e5
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-05-20 20:20:39 +0200

    feat(waybill): generate api docs

--- fluctuat-api/src/routes/public/public-waybill.route.ts
+++ fluctuat-api/src/routes/public/public-waybill.route.ts
@@ -9,6 +9,14 @@ import { verifyApiKey } from './verify-api-key.middleware';
 const publicWaybillRoute = Router();
 
 /**
+ * @api {get} /waybill/:id Get waybill
+ * @apiGroup Waybill
+ * @apiName GetWaybill
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} id waybill id
+ *
+ * @apiSuccess {Waybill} waybill the requested waybill
  *
  */
 publicWaybillRoute.get('/:id', verifyApiKey, fetchWaybill, (req: WaybillRequest, res) => {
@@ -16,7 +24,10 @@ publicWaybillRoute.get('/:id', verifyApiKey, fetchWaybill, (req: WaybillRequest,
 });
 
 /**
- *
+ * @api {post} /waybill create new waybill
+ * @apiGroup Waybill
+ * @apiName CreateWaybill
+ * @apiVersion 1.0.0
  */
 publicWaybillRoute.post('/', verifyApiKey, async (req: UserRequest, res) => {
   const userEmail = req.user.email;
@@ -28,7 +39,12 @@ publicWaybillRoute.post('/', verifyApiKey, async (req: UserRequest, res) => {
 });
 
 /**
+ * @api {put} /waybill/:id/order-info Modify existing waybill
+ * @apiGroup Waybill
+ * @apiName UpdateWaybillOrderInfo
+ * @apiVersion 1.0.0
  *
+ * @apiParam {String} id waybill id
  */
 publicWaybillRoute.put('/:id/order-info', verifyApiKey, fetchWaybill, async (req: WaybillRequest, res) => {
   const waybill: Waybill = req.waybill;
