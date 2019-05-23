e201c9d8c5134c6042928bb4d12fdacba38c85a7
Elias Boukamza <elias.boukamza@beta.gouv.fr>
2019-05-23 15:19:42 +0200

    feat(error component): make error component closable

--- fluctuat-web/src/app/shared/error.component.ts
+++ fluctuat-web/src/app/shared/error.component.ts
@@ -2,14 +2,23 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'flu-error',
-  template: `<div class="level" *ngIf="errorMsg"></div>
-             <div class="level" *ngIf="errorMsg">
-               <div class="level-item notification is-danger">{{errorMsg}}</div>
+  template: `<div class="level" style="margin-top: 1.5rem" *ngIf="errorMsg">
+               <div class="level-item notification is-danger">
+                 {{errorMsg}}
+                 <button class="delete" *ngIf="closable !== undefined" (click)="clearError()"></button>
+               </div>
              </div>`
 })
 export class ErrorComponent {
 
   @Input()
   errorMsg: string;
+
+  @Input()
+  closable: boolean;
+
+  clearError() {
+    this.errorMsg = undefined;
+  }
 
 }
